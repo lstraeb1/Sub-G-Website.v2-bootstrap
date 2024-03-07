15cafd16b40fa461bee9ffc36f63b56d0298e608
lstraeb1 <liamstraebler@gmail.com>
2024-03-06 22:25:42 -0500

    Updated mobile navbar

--- About-us-page/about-us.js
+++ About-us-page/about-us.js
@@ -1,13 +1,19 @@
 const body = document.body;
 const navToggleBtn = document.querySelector('.navbar-toggler');
-const navbar = document.getElementById('navbar');
-const navbarCollapse = document.querySelector('.collapse');
+const navbar = document.querySelector('.navbar');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const loginMobile = document.getElementById('log-in-mobile');
+const joinUsMobile = document.getElementById('join-us-mobile');  
 
 navToggleBtn.addEventListener('click', () => {
     if (navToggleBtn.classList.contains('collapsed')) {
       body.style.overflowY = 'auto';
+      loginMobile.classList.add('hidden');
+      joinUsMobile.classList.add('hidden');
     } else {
       body.style.overflowY = 'hidden';
+      loginMobile.classList.remove('hidden');
+      joinUsMobile.classList.remove('hidden');
     }
   });
 
