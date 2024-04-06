f5c223c40abf86bb11ff23f2c1285b1169a5e048
lstraeb1 <liamstraebler@gmail.com>
2024-04-06 16:20:25 -0400

    NA

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from 'next/app';
-import '../styles/globals.css'; // Import global styles here
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
